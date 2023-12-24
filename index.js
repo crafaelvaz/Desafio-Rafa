c07fbf4c70749b6f1baf2f9f5ab7b4874efcc75b
Rafael Vaz <150102822+crafaelvaz@users.noreply.github.com>
2023-12-24 00:19:04 -0300

    Finalizando projeto

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Olá, GitHub")
+let nomeHeroi = "Elthorn";
+let xpHeroi = 8500;
+let mensagem;
+
+if(xpHeroi < 1000) {
+    mensagem = "Ferro";
+}else if(xpHeroi >= 1001 && xpHeroi <= 2000) {
+    mensagem = "Bronze";
+}else if(xpHeroi >= 2001 && xpHeroi <= 5000) {
+    mensagem = "Prata";
+}else if(xpHeroi >= 5001 && xpHeroi <=7000) {
+    mensagem = "Ouro";
+}else if(xpHeroi >=7001 && xpHeroi <=8000) {
+    mensagem = "Platina";
+}else if(xpHeroi >=8001 && xpHeroi <=9000) {
+    mensagem = "Ascendente";
+}else if(xpHeroi >=9001 && xpHeroi <=10000) {
+    mensagem = "Imortal";
+}else {
+    mensagem = "Radiante";
+}console.log('O herói ${nomeHeroi} possui ${xpHeroi} XP e é classificado como ${mensagem}.');
